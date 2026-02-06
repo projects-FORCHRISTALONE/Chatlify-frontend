5f962db5d49669206fb26deca2c0df56b9aa1636
projects-FORCHRISTALONE <orimadegunemma@gmail.com>
2026-02-06 02:18:23 +0100

    chat-layout implemented

---
+++ src/store/useChatStore.js
@@ -0,0 +1,49 @@
+// ALL THANKS AND GLORY TO THE AND my ONLY GOD AND LORD JESUS CHRIST ALONE
+// BY GOD'S GRACE ALONE
+
+import { create } from "zustand";
+import { getAllContacts } from "../../../backend/src/controllers/message.controller";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set,get)=>({
+    allContacts: [],
+    chats : [],
+    messages : [],
+    activeTab : "chats",
+    selectedUser : null,
+    isUsersLoading : false,
+    isMessagesLoading : false,
+    isSoundEnabled : JSON.parse(localStorage.getItem("isSoundEnabled")) == true,
+
+    toggleSound: ()=>{
+        localStorage.setItem("isSoundEnabled", !get().isSoundEnabled);
+        set({isSoundEnabled: !get().isSoundEnabled});
+    },
+
+    setActiveTab: (tab) => set({activeTab : tab}),
+    setSelectedUser: (selectedUser) => set({selectedUser}),
+
+    getAllContacts: async () => {
+        set({isUsersLoading: true});
+        try{
+            const res = await axiosInstance.get("/messages/contacts");
+            set({allContacts : res.data})
+        } catch (err){
+            toast.err(err.response.data.message);
+        } finally {
+            set({isUsersLoading : false})
+        }
+    },
+
+    getMyChatPartners: async () => {
+        set({isUsersLoading : true});
+        try{
+            const res = await axiosInstance.get("messages/chats");
+            set({chats : res.data});
+        } catch( err) {
+            toast.error(err.response.data.message);
+        } finally {
+            set({isUsersLoading : false})
+        }
+    }
+}))
